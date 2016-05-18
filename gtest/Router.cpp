76a961f70f6a23b1e4473edda2ef1272ee230c7d
Michael Arnold <nim@shadowfire.org>
2016-05-18 00:07:56 -0500

    Adding Tests for calculating the host mask.

--- gtest/Router.cpp
+++ gtest/Router.cpp
@@ -21,6 +21,14 @@ TEST_F(RouterTest, TestHostMask)
 {
     uint64_t res = Network::getHostMask(0xFFFFFFFF, 24);
     ASSERT_EQ(0xFFFFFF00, res);
+    res = Network::getHostMask(0xFFFFFFFF, 16);
+    ASSERT_EQ(0xFFFF0000, res);
+    res = Network::getHostMask(0xFFFFFFFF, 8);
+    ASSERT_EQ(0xFF000000, res);
+    res = Network::getHostMask(0xFFFFFFFF, 32);
+    ASSERT_EQ(0xFFFFFFFF, res);
+    res = Network::getHostMask(3232243726ul, 30);
+    ASSERT_EQ(3232243724ul, res);
 }
 
 /**
